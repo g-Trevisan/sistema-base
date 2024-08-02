d7682897c862c4ac8f5f3e62dbfd84b4fff5c4b9
g-Trevisan <gtrevis15@gmail.com>
2024-08-01 22:18:23 -0300

    adicionando e configurando o exemplo do react router dom 6

--- sistema-base/src/routes/index.tsx
+++ sistema-base/src/routes/index.tsx
@@ -1,1 +1,11 @@
-export {};
+import { Navigate, Route, Routes } from "react-router-dom"; // importa os componentes necessários da biblioteca react-router-dom
+
+export const AppRoutes = () => { // definindo o componente AppRoutes
+
+    return (
+        <Routes>
+            <Route path="/pagina-inicial" element={<p>Página inicial</p>} /> {/* define a rota para a página inicial da aplicação, que renderiza um elemento de parágrafo do HTML */}
+            <Route path="*" element={<Navigate to="/pagina-inicial"/>} />  {/* define uma rota padrão que redireciona para a página inicial caso a URL não corresponda a nenhuma rota definida */}
+        </Routes>
+    );
+}
