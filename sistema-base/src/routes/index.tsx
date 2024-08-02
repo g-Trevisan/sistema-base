0e70ccd65471e14ff376f64ddaa4396331b77ead
g-Trevisan <gtrevis15@gmail.com>
2024-08-02 00:01:21 -0300

    instalando o material ui

--- sistema-base/src/routes/index.tsx
+++ sistema-base/src/routes/index.tsx
@@ -1,11 +1,12 @@
+import { Button } from '@mui/material'; // importando o Button do materialui
 import { Navigate, Route, Routes } from "react-router-dom"; // importa os componentes necessários da biblioteca react-router-dom
 
 export const AppRoutes = () => { // definindo o componente AppRoutes
 
     return (
         <Routes>
-            <Route path="/pagina-inicial" element={<p>Página inicial</p>} /> {/* define a rota para a página inicial da aplicação, que renderiza um elemento de parágrafo do HTML */}
-            <Route path="*" element={<Navigate to="/pagina-inicial"/>} />  {/* define uma rota padrão que redireciona para a página inicial caso a URL não corresponda a nenhuma rota definida */}
+            <Route path="/pagina-inicial" element={<Button>te</Button>} /> {/* define a rota para a página inicial da aplicação, que renderiza um elemento de parágrafo do HTML */}
+            <Route path="*" element={<Navigate to="</pagina-inicial>"/>} />  {/* define uma rota padrão que redireciona para a página inicial caso a URL não corresponda a nenhuma rota definida */}
         </Routes>
     );
 }
