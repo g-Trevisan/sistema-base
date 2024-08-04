d2c4dcb74e26668346e48c5c0539009074971011
g-Trevisan <gtrevis15@gmail.com>
2024-08-04 15:55:53 -0300

    Adicionado o tema claro com o MaterialUI ThemeProvider

--- sistema-base/src/routes/index.tsx
+++ sistema-base/src/routes/index.tsx
@@ -5,8 +5,8 @@ export const AppRoutes = () => { // definindo o componente AppRoutes
 
     return (
         <Routes>
-            <Route path="/pagina-inicial" element={<Button>te</Button>} /> {/* define a rota para a página inicial da aplicação, que renderiza um elemento de parágrafo do HTML */}
-            <Route path="*" element={<Navigate to="</pagina-inicial>"/>} />  {/* define uma rota padrão que redireciona para a página inicial caso a URL não corresponda a nenhuma rota definida */}
+            <Route path="/pagina-inicial" element={<Button variant='contained' color='primary'>te</Button>} /> {/* define a rota para a página inicial da aplicação, que renderiza um elemento de parágrafo do HTML */}
+            <Route path="*" element={<Navigate to="/pagina-inicia"/>} />  {/* define uma rota padrão que redireciona para a página inicial caso a URL não corresponda a nenhuma rota definida */}
         </Routes>
     );
 }
